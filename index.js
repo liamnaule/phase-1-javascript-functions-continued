8e81693909011ccb8d87545893f6a9b150a17a14
liamnaule <liamnaule@gmail.com>
2025-03-13 01:07:08 +0300

    completed functions continued lab

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // code your solution here
+// 1. saturdayFun function
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+  }
+  
+  // 2. mondayWork function
+  function mondayWork(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+  }
+  
+  // 3. wrapAdjective function
+  function wrapAdjective(flair = "*") {
+    return function (adjective = "special") {
+      return `You are ${flair}${adjective}${flair}!`;
+    };
+  }
+  
+  // Testing the functions
+  console.log(saturdayFun()); // "This Saturday, I want to roller-skate!"
+  console.log(saturdayFun("bathe my dog")); // "This Saturday, I want to bathe my dog!"
+  
+  console.log(mondayWork()); // "This Monday, I will go to the office."
+  console.log(mondayWork("work from home")); // "This Monday, I will work from home."
+  
+  const encouragingPromptFunction = wrapAdjective("!!!");
+  console.log(encouragingPromptFunction("a dedicated programmer")); // "You are !!!a dedicated programmer!!!"
+  
+  const wrapWithDollars = wrapAdjective("$$");
+  console.log(wrapWithDollars("a winner")); // "You are $$a winner$$!"
+  
